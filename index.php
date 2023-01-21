0a39d61df02990df26bba853c5e8f12c0a332df2
Chad <219383847@mycput.ac.za>
2023-01-21 21:37:20 +0200

    made changes to file

--- index.php
+++ index.php
@@ -22,63 +22,56 @@
     <title>Product List</title>
 </head>
 <body>
-    <h1>Product List</h1>
-    <div>
-<form
-    action="src/php/controllers/MassDelete.php"
-    method="post"
-    id="product_list_form"
->
-<table id="product-table">
-    <tbody class="table-body">
-        <?php 
-        $database = new Database();
-        $db = $database->getConnection();
-        $product = new Product($db);
-        $productTypes = new ProductTypes($db);
-        $types = $productTypes->read();
-        $products = $product->readAll();
-
-        foreach($products as $product) {
-            echo "<tr class='product-row'>";
-            echo "<td>" . $product['sku'] . "</td>";
-            echo "<td>" . $product['name'] . "</td>";
-            echo "<td>" . $product['price'] . "</td>";
-            echo "<td>";
-            if (!empty($product['attributes'])) {
-                if($product['type']==='3'){
-                    echo "dimensions: " . $product['attributes']['height'] . "x" . $product['attributes']['width'] . "x" . $product['attributes']['length'] . "<br>";
-                }
-                else{
-                    foreach($product['attributes'] as $attribute_name => $attribute_value) {
-                        echo $attribute_name . ": " . $attribute_value . "<br>";
+<main class="main-container">
+    <div class="product-header">
+        <h1>Product List</h1>
+        <div>
+            <a href="src/views/AddProduct.php" class="add-btn">ADD</a>
+            <input type="submit" value="MASS DELETE" class="ms-dlt-btn" id="mass-delete-btn">
+        </div>
+    </div>
+    <form action="src/php/controllers/MassDelete.php" method="post" id="product_list_form">
+        <div class="table-body">
+            <?php
+                    $database = new Database();
+                    $db = $database->getConnection();
+                    $product = new Product($db);
+                    $productTypes = new ProductTypes($db);
+                    $types = $productTypes->read();
+                    $products = $product->readAll();
+            foreach($products as $product) {
+                echo "<div class='table-body-item'>";
+                echo "<p>" . $product['sku'] . "</p>";
+                echo "<p>" . $product['name'] . "</p>";
+                echo "<p>" . $product['price'] . " $</p>";
+                if (!empty($product['attributes'])) {
+                    if($product['type']==='3'){
+                        echo "<p>dimensions: " . $product['attributes']['height'] . "x" . $product['attributes']['width'] . "x" . $product['attributes']['length'] . "</p>";
+                        echo "<input type='checkbox' class='delete-checkbox' value='{$product['sku']}'>";
+                    }
+                    else{
+                        foreach($product['attributes'] as $attribute_name => $attribute_value) {
+                            echo "<p>" . $attribute_name . ": ";
+                            if ($attribute_name === 'size') {
+                                echo $attribute_value . "MB";
+                            } else if($attribute_name === 'weight') {
+                                echo $attribute_value . "KG";
+                            }
+                            echo "</p>";
+                            echo "<input type='checkbox' class='delete-checkbox' value='{$product['sku']}'>";
+                        }
                     }
                 }
+                echo "</div>";
             }
-            echo "</td>";
-            echo "</tr>";
-        }
-
-        // while ($row = $products->fetch(PDO::FETCH_ASSOC)){
-        //     extract($row);
-        //     echo "<tr class='product-row'>";
-        //         echo "<td>{$sku}</td>";
-        //         echo "<td>{$name}</td>";
-        //         echo "<td>{$price} $</td>";
-        //         echo "<td>{$type}</td>";
-        //         echo "<td><input type='checkbox' class='delete-checkbox' value='{$sku}'></td>";
-        //     echo "</tr>";
-        // }
-        ?>
-    </tbody>
-</table>
-    <input type="submit" value="MASS DELETE" id="mass-delete-btn">
-    <a href="src/views/AddProduct.php">ADD</a>
-    <h1 id="message">Types</h1>
-    <?php echo json_encode($types) ?>
-    <h1>Data</h1>
-    <?php echo json_encode($product->readAll()) ?>
-</form>
+            ?>
+        </div>
+        <h1 id="message">Types</h1>
+        <?php echo json_encode($types) ?>
+        <h1>Data</h1>
+        <?php echo json_encode($products) ?>
+    </form>
+<main>
 <script src="src/views/js/index.js"></script>
 </body> 
 </html>
