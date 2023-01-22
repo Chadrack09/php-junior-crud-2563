8f1b77e5ac5e0ddecc79ab7574d9f424d298342d
Chad <219383847@mycput.ac.za>
2023-01-22 04:54:55 +0200

    made changes to sql file

--- src/php/config/Query.sql
+++ src/php/config/Query.sql
@@ -26,8 +26,6 @@ CREATE TABLE products (
     name VARCHAR(255) NOT NULL,
     price DECIMAL(10,2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    -- type ENUM('DVD', 'Book', 'Furniture') NOT NULL,
-    -- FOREIGN KEY (type) REFERENCES product_types(type) ON DELETE CASCADE
     type INT NOT NULL,
     FOREIGN KEY (type) REFERENCES product_types(id)
 );
@@ -92,3 +90,23 @@ INSERT INTO book (product_id, weight) VALUES (LAST_INSERT_ID(), '0.4');
 
 INSERT INTO products (sku, name, price, type) VALUES ('furniture002', 'Dining Table', '799.99', 3);
 INSERT INTO furniture (product_id, height, width, length) VALUES (LAST_INSERT_ID(), '0.7', '1.5', '2.2');
+
+-- Another Insert Statements
+INSERT INTO products (sku, name, price, type) VALUES ('dvd003', 'The Club of Three', '8.99', 1);
+INSERT INTO dvd (product_id, size) VALUES (LAST_INSERT_ID(), '2.4');
+
+INSERT INTO products (sku, name, price, type) VALUES ('book003', 'The Beast in The Jungle', '10.50', 2);
+INSERT INTO book (product_id, weight) VALUES (LAST_INSERT_ID(), '0.2');
+
+INSERT INTO products (sku, name, price, type) VALUES ('furniture003', 'Office Chair', '350.99', 3);
+INSERT INTO furniture (product_id, height, width, length) VALUES (LAST_INSERT_ID(), '0.4', '1.0', '1.9');
+
+-- Another Insert Statements
+INSERT INTO products (sku, name, price, type) VALUES ('dvd004', 'Singing A Song', '12.85', 1);
+INSERT INTO dvd (product_id, size) VALUES (LAST_INSERT_ID(), '10.4');
+
+INSERT INTO products (sku, name, price, type) VALUES ('book004', 'Marry The Princess', '20.99', 2);
+INSERT INTO book (product_id, weight) VALUES (LAST_INSERT_ID(), '0.8');
+
+INSERT INTO products (sku, name, price, type) VALUES ('furniture004', 'Suit Case', '620.99', 3);
+INSERT INTO furniture (product_id, height, width, length) VALUES (LAST_INSERT_ID(), '1.2', '0.3', '0.6');
