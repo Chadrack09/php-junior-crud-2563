8fb6784191de87e5a00c2bc84fca75fc927f6046
Chad <219383847@mycput.ac.za>
2023-03-04 19:34:14 +0200

    made changes to producy types php file

--- src/php/models/ProductTypes.php
+++ src/php/models/ProductTypes.php
@@ -1,29 +1,10 @@
 <?php
 
-    class ProductTypes {
-        private $conn;
-        private $table_name = "product_types";
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/src/php/models/Product.php');
+
+    class ProductTypes extends Product {
          
         public function __construct($db) {
-            $this->conn = $db;
-        }
-
-        public function read() {
-            $query = "SELECT pt.id, pt.type, GROUP_CONCAT(pa.name) as attribute_names, pa.unit
-                      FROM product_types pt
-                      JOIN product_type_attributes pta ON pt.id = pta.product_type_id
-                      JOIN product_attributes pa ON pta.attribute_id = pa.id
-                      GROUP BY pt.id";
-            $stmt = $this->conn->prepare($query);
-            $stmt->execute();
-            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            foreach ($result as &$product_type) {
-                $product_type['attribute_names'] = explode(',', $product_type['attribute_names']);
-            }
-
-            return $result;
+            parent::__construct($db);
         }
     }
-
-?>
